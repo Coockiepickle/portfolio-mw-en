01d1b01f182656101e83e96ccd80acc8cf555200
Coockiepickle <dreynaud13@protonmail.com>
2025-03-01 09:56:29 +0100

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -22,7 +22,7 @@ const About = () => {
   const stats = [{
     icon: <Clock className="w-5 h-5 text-mw-green mr-2" />,
     value: "4+",
-    label: "Years Experience"
+    label: "Years Studied"
   }, {
     icon: <Target className="w-5 h-5 text-mw-green mr-2" />,
     value: "10+",
