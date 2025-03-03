0f6bddca24bc18b472755922aa25669d15bf7ace
Coockiepickle <dreynaud13@protonmail.com>
2025-03-03 15:54:50 +0100

    Update projects.ts

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -2,24 +2,24 @@
 import { ProjectData } from '../components/projects/ProjectCard';
 
 export const projectsData: ProjectData[] = [{
-  title: "S.P.P. (Système de Pentesting Portatif)",
-  description: "A standalone Raspberry Pi board for auditing and carrying out penetration tests on a network.",
+  title: "H.P.S. (Handheld Pentesting System)",
+  description: "A standalone Raspberry Pi board for auditing and carrying out penetration tests on a network including a battery and a screen.",
   tags: ["RPi", "Debian", "Lynis", "ZPhisher", "PhishMailer", "VNC"],
   type: "professional",
   date: "2023-09",
   links: {
-    demo: "https://dreynaud.noho.st/grav",
+    demo: "https://dreynaud.noho.st/grav/Cegep/ESP",
     github: "#"
   }
 }, {
-  title: "Combat Mission Planner",
-  description: "Interactive mission planning tool with real-time collaboration features.",
-  tags: ["Node.js", "WebSockets", "Canvas API"],
+  title: "Self-hosted Services",
+  description: "Self-hosted NAS, VPN, Web Server, Knowledge Base and other Web Apps accessible through the Internet",
+  tags: ["YunoHost", "CasaOS", "Firefly", "Grav", "No-IP"],
   type: "professional",
   date: "2023-03",
   links: {
-    demo: "#",
-    github: "#"
+    demo: "dreynaud.noho.st",
+    github: "https://github.com/YunoHost"
   }
 }, {
   title: "RestoRate",
@@ -32,10 +32,10 @@ export const projectsData: ProjectData[] = [{
     github: "https://github.com/Coockiepickle/PIW_EXS"
   }
 }, {
-  title: "SerreConnectée",
+  title: "Smart Greenhouse",
   description: "A collection of scripts used to create a connected greenhouse with sensors and an Arduino board..",
   tags: ["C++", "Python", "Arduino"],
-  type: "personal",
+  type: "professional",
   date: "2022-07",
   links: {
     demo: "#",
