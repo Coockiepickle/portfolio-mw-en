398b63d6feb7b392afa773302ce0df9e183adae8
Damien Reynaud <dreynaud13@protonmail.com>
2025-03-20 09:13:25 +0100

    Update HeroDescription.tsx

--- src/components/hero/HeroDescription.tsx
+++ src/components/hero/HeroDescription.tsx
@@ -5,7 +5,7 @@ interface HeroDescriptionProps {
 const HeroDescription = ({
   loaded
 }: HeroDescriptionProps) => {
-  return <p className={cn("text-lg md:text-xl max-w-2xl mx-auto mb-8 leading-relaxed transition-all duration-700 delay-300 ease-out transform", loaded ? "opacity-100 translate-y-0" : "opacity-0 -translate-y-8")}>...with both precision and strategic approach.
+  return <p className={cn("text-lg md:text-xl max-w-2xl mx-auto mb-8 leading-relaxed transition-all duration-700 delay-300 ease-out transform", loaded ? "opacity-100 translate-y-0" : "opacity-0 -translate-y-8")}>...with both precision and strategic approach.<br>
 Curious and attentive person with an ability to adapt and a strong intellectual curiosity. </p>;
 };
 export default HeroDescription;
